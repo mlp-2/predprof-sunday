2e990adb91847c384edbfc3b0b205d752911e929
Денис Войтенко <dv.voitenko@outlook.com>
2023-03-18 22:10:36 +0300

    Добавление ASP.NET Core

---
+++ PredprofAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PredprofAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
